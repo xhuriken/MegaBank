7e9327bce9175b4e51760f4bc740f11d3d532878
xhuriken <73635763+xhuriken@users.noreply.github.com>
2025-10-13 16:31:46 +0200

    Init project MEGABANK

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from typing import TypedDict
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue sur FastAPI!"}
+
