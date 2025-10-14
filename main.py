2c3bfe2b1dd1359be561c0c4b0d2868d6f177a9a
L0ll0w <matheo.beaudet@edu.esiee-it.fr>
2025-10-14 15:38:21 +0200

    connexion/inscription

--- main.py
+++ main.py
@@ -3,79 +3,146 @@ from pydantic import BaseModel
 from typing import TypedDict
 from sqlmodel import Field, SQLModel, Session, create_engine, select
 
-#pydantic gestion de model
-
-#GESTION DERREUR: raise HTTPException(status_code=400, detail="ERREUR")
-
-#Liste
-mon_tab = [5, 8, 6, 9]
-m = [[1, 3, 4],
-     [5, 6, 8],
-     [2, 1, 3],
-     [7, 8, 15]]
-
-#Dictionaire
-tableau = {
-    1: 50.0,
-    2: 3.0
-}
-#Dictionaire
-tableau2con2 = {
-    1: {"name": "Fabrice",      "id": 1},
-    2: {"name": "Emile louis",  "id": 2},
-    3: {"name": "éboué",        "id": 3},
-}
-
-
-
-
-
-
-
-
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue sur FastAPI!"}
 
+class User():
+    id: int
+    firstName: str
+    lastName: str
+    email: str
+    password: str
 
-class Account(BaseModel):
-    name: str
+    def __init__(self, id, firstName, lastName, email,password):
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+
+class Account():
+    iban: str
     balance: float
+    userId: int
 
-account = Account(name="Fabrice", balance=100)
-account2 = Account(name="Eboue", balance=5)
+    def __init__(self, iban, balance, userId):
+        # c'est pour la bdd ça self.id = id
+        self.iban = iban
+        self.balance = balance
+        self.userId = userId
+
+users = {
+    1 : User(1, "Jean", "1", "email","gyuezgef"),
+    2 : User(2, "Fabrice", "2", "email","giugyyig"),
+    3 : User(3, "test", "test", "email","huihuh"),
+}
+
+accounts = {
+    1 : Account("FR 0", 10, "email"),
+    2 : Account("FR 1", 100, "email"),
+    3 : Account("FR 2", 30, "email"),
+}
+
+class UserBody(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+
+@app.post("/register")
+def register_user(user_data: UserBody):
+    new_id = max(users.keys()) + 1
+
+    for u in users.values():
+        if u.email == user_data.email:
+            raise HTTPException(status_code=400, detail="Sa existe deja nullos")
+
+    new_user = User(
+    id=new_id,
+    firstName=user_data.first_name,
+    lastName=user_data.last_name,
+    email=user_data.email,
+    password=user_data.password 
+    )
+
+    users[new_id] = new_user
+    return {
+    "message": "Tu existe maintenant",
+    "first_name": new_user.firstName,
+    "last_name": new_user.lastName
+}
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+typeUserActual = None
+
+@app.post("/login")
+def login_user(credentials: UserLogin):
+    global userAcutal
+
+    for u in users.values():
+        if u.email == credentials.email and u.password == credentials.password:
+            typeUserActual = u  
+           
+            return {
+                "message": f"Bienvenue {u.firstName}, t’es vraiment le GOAT !",
+                "user": typeUserActual
+            }
+
+    raise HTTPException(status_code=401, detail="Identifiants invalides")
 
 @app.get("/get_balance")
-def get_balance(acc: Account):
-    return {"solde": acc.balance}
+def get_balance(iban: str):
+    for a in accounts.values():
+        if(a.iban == iban):
+            return {"solde":a.balance, "Iban" : iban}
+    return "Iban ne exista"
+
 
 @app.post("/deposit")
-def deposit(amount: float, acc: Account):
-    acc.balance += amount
-    return {"solde": acc.balance}
+def deposit(amount: float, iban: str):
+    for a in accounts.values():
+        if(a.iban == iban):
+            a.balance += amount
+            return {"solde": a.balance}
+    return {"Iban no exista"}
+
 
 @app.post("/withdraw")
-def withdraw(amount: float, acc: Account):
-    if(acc.balance < amount):
-        raise HTTPException(status_code=400, detail="T'a pas la thune")
-    else:
-        acc.balance -= amount
-    return {"solde": acc.balance}
+def withdraw(amount: float, iban: str):
+    for a in accounts.values():
+        if(a.iban == iban):
+            if(a.balance < amount):
+                raise HTTPException(status_code=400, detail="T'a pas la thune")
+            else:
+                a.balance -= amount
+            return {"solde": a.balance}
+    return {"Iban no exista"}
+
 
 @app.post("/transit")
-def transit(amount: float, sender: Account, receiver: Account):
+def transit(amount: float, sender: str, receiver: str):
     withdraw(amount, sender)
     deposit(amount, receiver)
-    return {"Receiver": receiver.balance, "Sender" : sender.balance}
+    return {"Receiver": get_balance(receiver), "Sender" : get_balance(sender)}
 
 @app.get("/me")
-def account_info(acc: Account):
-    return {"Name: " : acc.name, "Balance" : acc.balance}
+def user_info(usrId: int):
+    for i in users.values():
+        if(i.id == usrId):
+            return {"First Name: " : i.firstName, "Last Name" : i.lastName, "Email" : i.email}
+    return {"user no exista"}    
+    
 
+@app.get("/account")
+def account_info(iban: str):
+    for a in accounts.values():
+        if(a.iban == iban):
+            return {"IBAN : " : iban, "Balance " : a.balance}
+    return {"Iban no exista"}
 
-#this working heoryaeporuhaer
-transit(5, account, account2)
-#python ça pu wallah
-print(account_info(account) , " | " , account_info(account2))
