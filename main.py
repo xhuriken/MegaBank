84720788425147c69f4cb51b397031523052c861
jean-killian <killian.goulet.dubasque@gmail.com>
2025-10-14 15:50:52 +0200

    add open_account function

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ class User():
 class Account():
     iban: str
     balance: float
+    isPrimary: bool
     userId: int
 
-    def __init__(self, iban, balance, userId):
+    def __init__(self, iban, balance, isPrimary, userId):
         # c'est pour la bdd ça self.id = id
         self.iban = iban
         self.balance = balance
+        self.isPrimary = isPrimary
         self.userId = userId
 
 users = {
@@ -39,10 +41,49 @@ users = {
 }
 
 accounts = {
-    1 : Account("FR 0", 10, "email"),
-    2 : Account("FR 1", 100, "email"),
-    3 : Account("FR 2", 30, "email"),
+    1 : Account("FR 0", 10, True, 1),
+    2 : Account("FR 1", 100, True, 2),
+    3 : Account("FR 2", 30, False, 1),
+    4 : Account("FR 3", 30, False, 1),
+    5 : Account("FR 4", 30, False, 1),
+    6 : Account("FR 5", 30, False, 1),
 }
+@app.post("/open_account")
+def open_account(userId: int):
+
+    #check if user exist
+    if userId not in users:
+        raise HTTPException(status_code=404, detail="Utilisateur inexistant")
+
+    balance = 0
+    nb_found_accounts = 0
+    isPrimary = False
+
+    for bite in accounts.values():
+        if bite.userId == userId:
+            nb_found_accounts += 1
+
+    if nb_found_accounts >= 5:
+        return {"message": "Arrête de créer des comptes t'en à déjà trop"}
+
+    if nb_found_accounts == 0:
+        balance = 100
+        isPrimary = True
+
+    new_id = max(accounts.keys()) + 1
+    iban = f"FR {new_id}"
+    new_account = Account(iban, balance, isPrimary, userId)
+    accounts[new_id] = new_account
+
+    return {
+        "message": "Compte créé avec succès",
+        "id": new_id,
+        "iban": new_account.iban,
+        "balance": new_account.balance,
+        "userId": new_account.userId
+    }
+
+
 
 @app.get("/get_balance")
 def get_balance(iban: str):
@@ -91,5 +132,5 @@ def user_info(usrId: int):
 def account_info(iban: str):
     for a in accounts.values():
         if(a.iban == iban):
-            return {"IBAN : " : iban, "Balance " : a.balance}
+            return {"IBAN : " : iban, "Balance " : a.balance, "isPrimary " : a.isPrimary, "userId" : a.userId}
     return {"Iban no exista"}
