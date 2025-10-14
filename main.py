cf12a1bf927e00e78f8a0055b9467b189400d25f
xhuriken <73635763+xhuriken@users.noreply.github.com>
2025-10-14 16:17:04 +0200

    Starting refarctoring

--- main.py
+++ main.py
@@ -33,6 +33,80 @@ class Account():
         self.balance = balance
         self.userId = userId
 
+    def deposit(self, amount: float):
+        if amount <= 0:
+            raise ValueError("Amount need to be > 0")
+        self.balance += amount
+
+    def withdraw(self, amount: float):
+        if amount <= 0:
+            raise ValueError("Amount need to be > 0")
+        if self.balance < amount:
+            raise ValueError("Not enought bonk")
+        self.balance -= amount
+        
+
+def get_acc(iban: str) -> Account:
+    acc = accounts.get(iban)
+    if not acc:
+        raise HTTPException(404, "Iban not found")
+    return acc
+
+
+
+#MOVE in Utils class why not ?
+@app.get("/balance/{iban}")
+def get_balance(iban: str):
+    acc = get_acc(iban)
+    return {"iban": acc.iban, "balance": acc.balance}
+
+
+@app.post("/deposit/{iban}")
+def deposit(iban: str, amount: float):
+    acc = get_acc(iban)
+    try:
+        #Deposit can make error "Value Error" go see in the Account class
+        acc.deposit(amount)
+    except ValueError as e:
+        #If it this case, throw HTTPException and print the exeption
+        raise HTTPException(400, str(e))
+    return {"iban": acc.iban, "balance": acc.balance}
+
+
+@app.post("/withdraw/{iban}")
+def withdraw(iban: str, amount: float):
+    acc = get_acc(iban)
+    try:
+        acc.withdraw(amount)
+    except ValueError as e:
+        raise HTTPException(400, str(e))
+    return {"iban": acc.iban, "balance": acc.balance}
+
+@app.post("/transit")
+def transit(amount: float, sender: Account, receiver: Account):
+    withdraw(amount, sender)
+    deposit(amount, receiver)
+    return {"Receiver": receiver.balance, "Sender" : sender.balance}
+
+#GESTION USER
+
+@app.get("/me")
+def user_info(usrId: int):
+    for i in users.values():
+        if(i.id == usrId):
+            return {"First Name: " : i.firstName, "Last Name" : i.lastName, "Email" : i.email}
+    return {"user no exista"}    
+    
+
+@app.get("/account")
+def account_info(iban: str):
+    for a in accounts.values():
+        if(a.iban == iban):
+            return {"IBAN : " : iban, "Balance " : a.balance}
+    return {"Iban no exista"}
+
+
+
 users = {
     1 : User(1, "Jean", "1", "email","gyuezgef"),
     2 : User(2, "Fabrice", "2", "email","giugyyig"),
@@ -40,16 +114,23 @@ users = {
 }
 
 accounts = {
-    1 : Account("FR 0", 10, "email"),
-    2 : Account("FR 1", 100, "email"),
-    3 : Account("FR 2", 30, "email"),
+    "FR 0" : Account("FR 0", 10, "email"),
+    "FR 1" : Account("FR 1", 100, "email"),
+    "FR 2" : Account("FR 2", 30, "email"),
 }
+
+
 
 class UserBody(BaseModel):
     first_name: str
     last_name: str
     email: str
     password: str
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+typeUserActual = None
 
 @app.post("/register")
 def register_user(user_data: UserBody):
@@ -74,12 +155,6 @@ def register_user(user_data: UserBody):
     "last_name": new_user.lastName
 }
 
-class UserLogin(BaseModel):
-    email: str
-    password: str
-
-typeUserActual = None
-
 @app.post("/login")
 def login_user(credentials: UserLogin):
     global userAcutal
@@ -94,41 +169,3 @@ def login_user(credentials: UserLogin):
             }
 
     raise HTTPException(status_code=401, detail="Identifiants invalides")
-
-@app.get("/get_balance")
-def get_balance(acc: Account):
-    return {"solde": acc.balance}
-
-@app.post("/deposit")
-def deposit(amount: float, acc: Account):
-    acc.balance += amount
-    return {"solde": acc.balance}
-
-@app.post("/withdraw")
-def withdraw(amount: float, acc: Account):
-    if(acc.balance < amount):
-        raise HTTPException(status_code=400, detail="T'a pas la thune")
-    else:
-        acc.balance -= amount
-    return {"solde": acc.balance}
-
-@app.post("/transit")
-def transit(amount: float, sender: Account, receiver: Account):
-    withdraw(amount, sender)
-    deposit(amount, receiver)
-    return {"Receiver": receiver.balance, "Sender" : sender.balance}
-
-@app.get("/me")
-def user_info(usrId: int):
-    for i in users.values():
-        if(i.id == usrId):
-            return {"First Name: " : i.firstName, "Last Name" : i.lastName, "Email" : i.email}
-    return {"user no exista"}    
-    
-
-@app.get("/account")
-def account_info(iban: str):
-    for a in accounts.values():
-        if(a.iban == iban):
-            return {"IBAN : " : iban, "Balance " : a.balance}
-    return {"Iban no exista"}
